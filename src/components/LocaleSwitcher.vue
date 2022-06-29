12080c6dc881196f173ea028c8aed2f53d77e56e
Javier Sánchez <javiersanp@gmail.com>
2022-06-30 00:57:03 +0100

    Añade i18n

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useI18n } from "vue-i18n";
+
+const i18n = useI18n();
+
+i18n.locale.value = localStorage.getItem("locale") || "es_ES";
+
+function saveLocale() {
+  console.info(i18n.locale.value);
+  localStorage.setItem("locale", i18n.locale.value);
+}
+</script>
+
+<template>
+  <div class="select">
+    <select v-model="$i18n.locale" @change="saveLocale">
+      <option
+        v-for="locale in $i18n.availableLocales"
+        :key="`locale-${locale}`"
+        :value="locale"
+      >
+        {{ locale.substring(0, 2).toUpperCase() }}
+      </option>
+    </select>
+  </div>
+</template>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: "es_ES",
+      fallbackLocale: "es_ES",
+      localeDir: "locales",
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true,
+    },
+  },
 });
