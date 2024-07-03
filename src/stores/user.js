d87d004391fa2263d48adf37d03d0ec0f5e09c63
Javier Sánchez Portero <frsanchezp@aemet.es>
2024-07-03 21:18:48 +0100

    Corrige inclusión de token en llamadas api

--- src/stores/user.js
+++ src/stores/user.js
@@ -5,8 +5,9 @@ import { reject } from 'lodash'
 export const useUserStore = defineStore({
   id: 'user',
   state: () => ({
-    username: '',
-    osmId: 0,
+    username: localStorage.getItem('username') || '',
+    osmId: localStorage.getItem('osmId') || 0,
+    token: localStorage.getItem('token') || '',
   }),
 
   getters: {
@@ -25,6 +26,10 @@ export const useUserStore = defineStore({
           .then((response) => {
             this.username = response.data.username || ''
             this.osmId = response.data.osm_id || 0
+            this.token = response.data.session_token || ''
+            api.defaults.headers.common[
+              'Authorization'
+            ] = `bearer ${this.token}`
             resolve(response)
           })
           .catch(() => {
